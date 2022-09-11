b293c6a58afa592b2b302bdcdf66e0b25f01b74a
arabath <axelhobrecht@hotmail.com>
2022-09-11 18:35:19 -0300

    Mostrar las opciones posibles

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -14,20 +14,20 @@ const getPokemonOptions = async() => {
 
     const pokemons = await getPokemonNames( mixedPokemons.splice(0, 4) );
 
-    console.table(pokemons)
+    return pokemons
 };
 
-const getPokemonNames = async ([a, b, c, d] = []) => {
+const getPokemonNames = async ([a,b,c,d] = []) => {
 //   const resp = await pokemonApi.get(`/1`);
 //   console.log(resp);
-const promisesArr = [
+const promiseArr = [
     pokemonApi.get(`/${ a }`),
     pokemonApi.get(`/${ b }`),
     pokemonApi.get(`/${ c }`),
     pokemonApi.get(`/${ d }`)
 ]
 
-const [p1, p2, p3, p4 ] = await Promise.all( promisesArr )
+const [p1, p2, p3, p4 ] = await Promise.all( promiseArr )
 return [
     { name: p1.data.name, id: p1.data.id },
     { name: p2.data.name, id: p2.data.id },
--- src/pages/pokemonPage.vue
+++ src/pages/pokemonPage.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>¿Quién es este pokémon?</h1>
 
-    <PokemonPicture :pokemonId="4" :showPokemon="true"/>
-    <PokemonOptions />
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+
+  <div v-else>
+    <h1>¿Quién es este pokémon?</h1>
+
+      <PokemonPicture :pokemonId="pokemon.id" :showPokemon="true"/>
+      <PokemonOptions :pokemons="pokemonArr"/>
+  </div>
 </template>
 
 <script>
@@ -14,6 +19,24 @@ import getPokemonOptions from '@/helpers/getPokemonOptions'
 //console.log(getPokemonOptions())
 
 export default {
-    components: { PokemonOptions, PokemonPicture }
+    components: { PokemonOptions, PokemonPicture },
+    data() {
+      return {
+        pokemonArr: [],
+        pokemon: null
+      }
+    },
+    methods: {
+     async mixPokemonArray() {
+        this.pokemonArr = await getPokemonOptions()
+
+        const rndInt = Math.floor(Math.random() * 4 )
+        this.pokemon = this.pokemonArr[ rndInt ]
+        console.log(rndInt)
+      }
+    },
+    mounted() {
+      this.mixPokemonArray()
+    }
 }
 </script>
