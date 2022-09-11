f9265ce30f533136d520c93379164892a996e72a
arabath <axelhobrecht@hotmail.com>
2022-09-11 17:17:04 -0300

    Logica de los nombres de los pokemones

--- src/pages/pokemonPage.vue
+++ src/pages/pokemonPage.vue
@@ -1,16 +1,20 @@
+import PokemonPage from '.';
 <template>
   <h1>¿Quién es este pokémon?</h1>
   <!-- TODO: img -->
-
+    <PokemonPicture :pokemonId="4" :showPokemon="true"/>
   <!-- TODO: opciones -->
+    <PokemonOptions />
 </template>
 
 <script>
-export default {
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
 
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+console.log(getPokemonOptions)
+
+export default {
+    components: {PokemonOptions, PokemonPicture}
 }
 </script>
-
-<style>
-
-</style>
